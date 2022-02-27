9e2ea102610537e507a46874156875f96b3c9096
chandutejakarri <karrichandutejareddy@gmail.com>
2022-02-27 10:44:06 +0530

    addTwoNum.c

--- addTwoNum.c
+++ addTwoNum.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #define SUCCESS 0
 
+void modify(int *var_1,int *var_2)
+{
+	printf("Enter the updated two numbers: ");
+	scanf("%d %d",var_1,var_2);
+}
+
 int main()
 {
 	int a,b;
@@ -13,5 +19,7 @@ int main()
 	printf("%d\n",a-b);
 	/*division*/
 	printf("%d\n",a/b);
+	modify(&a,&b);
+	printf("Updated values of a and b is : %d %d\n",a,b);
 	return SUCCESS;
 }
